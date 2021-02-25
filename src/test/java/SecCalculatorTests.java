6997142c0c513edd2f8e2947d58c4dc808be9f90
littlePinkyDonkey <teplyh.andre@yandex.ru>
2021-02-25 13:56:34 +0300

    replaced all the @Test methods in the one @ParameterizedTest method in SecCalculatorTests.java

--- src/test/java/SecCalculatorTests.java
+++ src/test/java/SecCalculatorTests.java
@@ -1,17 +1,21 @@
 import first.SecCalculator;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
 import org.junit.jupiter.params.provider.CsvFileSource;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("First task tests")
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class SecCalculatorTests {
     private final SecCalculator calculator = new SecCalculator();
+    private final double DELTA = 0.001;
 
-    @DisplayName("Calculate sec(x)")
-    @ParameterizedTest(name = "sec({0})")
+    @ParameterizedTest(name = "Calculate sec({0})")
     @CsvFileSource(resources = "data.csv")
-    void calculate(Double param) throws InterruptedException {
-        System.out.println(calculator.calculateSec(param));
+    void test(Double expectedResult, Integer denominator) {
+        System.out.printf("res = %f, denominator = %d", expectedResult, denominator);
+        assertEquals(expectedResult, calculator.calculateSec(Math.PI / denominator), DELTA);
     }
 }
